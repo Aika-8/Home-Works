f651ee341db578adc9039778b064abedec34f310
Aiganysh <dzhumagulova8@gmail.com>
2025-02-10 15:14:18 +0300

    starting the footer part

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const main = document.querySelector("main");
 const lastEpisodes = [
   {
     images: "./assets/images/Бакас.jpg",
     star: "./assets/images/icons/star.svg",
     rating: "7.0",
-    title: "Бакас Алгачкы шоу-концерт",
+    title: "Бакас Алгачкы шоу...",
     status: "Аренда",
   },
   {
@@ -25,11 +24,11 @@ const lastEpisodes = [
     images: "./assets/images/Кыргызстан.jpg",
     star: "./assets/images/icons/star.svg",
     rating: "7.3",
-    title: "Кыргызстан, мен сени суйом",
+    title: "Кыргызстан, мен...",
     status: "Аренда",
   },
   {
-    images: "/assets/images/Акча.jpg",
+    images: "./assets/images/Акча.jpg",
     star: "./assets/images/icons/star.svg",
     title: "Акча",
     rating: "7.7",
@@ -43,7 +42,79 @@ const lastEpisodes = [
     status: "Абонемент",
   },
 ];
-lastEpisodes.forEach((item) => {
+const firstSection = document.getElementsByClassName("first_section");
+lastEpisodes.map((item) => {
+  const { images, star, rating, title, status } = item;
+  const div = document.createElement("div");
+  //   div.className = "last_Episode";
+  const image = document.createElement("img");
+  const divRating = document.createElement("div");
+  const starImg = document.createElement("img");
+  const pRating = document.createElement("p");
+  const titleP = document.createElement("p");
+  const titleStatus = document.createElement("p");
+  divRating.append(starImg, pRating);
+  //
+  image.className = "img_moveies";
+  image.src = images;
+  divRating.className = "divRating";
+  starImg.className = "image_star";
+  starImg.src = star;
+  pRating.className = "p_rating";
+  pRating.textContent = rating;
+  titleP.className = "p_title";
+  titleP.textContent = title;
+  titleStatus.className = "p_status";
+  titleStatus.textContent = status;
+  div.append(image, divRating, titleP, titleStatus);
+  firstSection[0].appendChild(div);
+});
+const serial = [
+  {
+    images: "./assets/images/Тайган.jpg",
+    star: "./assets/images/icons/star.svg",
+    rating: "8.6",
+    title: "Тайган",
+    status: "Аренда эпизода",
+  },
+  {
+    images: "./assets/images/Чоочун киши 2.jpg",
+    star: "./assets/images/icons/star.svg",
+    rating: "9.1",
+    title: "Чоочун киши 2",
+    status: "Аренда эпизода",
+  },
+  {
+    images: "./assets/images/Муз устундогу.jpg",
+    star: "./assets/images/icons/star.svg",
+    rating: "8.2",
+    title: "Муз усутундогу гулдор",
+    status: "Аренда эпизода",
+  },
+  {
+    images: "./assets/images/Жизнь.jpg",
+    star: "./assets/images/icons/star.svg",
+    rating: "7.5",
+    title: "Жизнь",
+    status: "Аренда эпизода",
+  },
+  {
+    images: "./assets/images/Ата-эне тилеги.jpg",
+    star: "./assets/images/icons/star.svg",
+    title: "Ата-эне тилеги",
+    rating: "7.3",
+    status: "Аренда эпизода",
+  },
+  {
+    images: "./assets/images/Чоочун киши 1.jpg",
+    star: "./assets/images/icons/star.svg",
+    rating: "9.1",
+    title: "Чоочун киши",
+    status: "Бесплатно",
+  },
+];
+const second_section = document.getElementsByClassName("second_section");
+serial.map((item) => {
   const { images, star, rating, title, status } = item;
   const div = document.createElement("div");
   const image = document.createElement("img");
@@ -52,42 +123,19 @@ lastEpisodes.forEach((item) => {
   const pRating = document.createElement("p");
   const titleP = document.createElement("p");
   const titleStatus = document.createElement("p");
-  div.className = "last_Episode";
-  div.style.width = "172px";
-  div.style.height = "370px";
-  div.style.marginRight = "30px";
-  div.style.backgroundColor = "rgb(16, 16, 16)";
-  div.style.display = "flex";
-  div.style.flexDirection = "column";
-  div.style.justifyContent = "center";
-  div.style.alignItems = "flex-start";
-  div.style.gap = "10px";
-  div.style.border = "2px solid blue";
-  image.className = "img_Last_Episode";
-  image.style.width = "169px";
-  image.style.height = "255px";
+  divRating.append(starImg, pRating);
+  //
+  image.className = "img_moveies";
   image.src = images;
   divRating.className = "divRating";
-  (divRating.style.width = "36px"), (divRating.style.height = "20px");
-  divRating.style.backgroundColor = "#22C55E";
-  divRating.style.display = "flex";
-  divRating.style.justifyContent = "center";
-  divRating.style.alignItems = "center";
-  divRating.style.gap = "5px";
-  starImg.style.width = "11px";
-  starImg.style.height = "12px";
+  starImg.className = "image_star";
   starImg.src = star;
-  pRating.style.fontSize = "10px";
-  pRating.style.color = "white";
-  pRating.style.marginTop = "2px";
-  pRating.style.fontWeight = "bold";
+  pRating.className = "p_rating";
   pRating.textContent = rating;
-  divRating.append(starImg, pRating);
-  titleP.style.color = "#E5E7EB";
+  titleP.className = "p_title";
   titleP.textContent = title;
-  titleStatus.style.fontSize = "12px";
-  titleStatus.style.color = "#3B82F6";
+  titleStatus.className = "p_status";
   titleStatus.textContent = status;
   div.append(image, divRating, titleP, titleStatus);
-  main.appendChild(div);
+  second_section[0].appendChild(div);
 });
