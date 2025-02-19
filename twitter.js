5449b102d1e51eea267cfb105bd0d06e36ff8684
Aiganysh <dzhumagulova8@gmail.com>
2025-02-19 23:02:03 +0300

    finishing

--- twitter.js
+++ twitter.js
@@ -1,7 +1,5 @@
 const body = document.getElementById("body");
 body.style.backgroundColor = "teal";
-body.style.display = "flex";
-body.style.gap = "50px";
 const socials = [
   {
     icon: "./assets/images/icons/scale_1200-Photoroom.png",
@@ -15,10 +13,6 @@ socials.forEach((item) => {
   const { icon, title, description, link } = item;
   const div1 = document.createElement("div");
   div1.className = "twitter1";
-  div1.style.width = "400px";
-  div1.style.height = "500px";
-  div1.style.backgroundColor = "white";
-  div1.style.border = "4px solid black";
   const iconimg = document.createElement("img");
   iconimg.style.width = "100px";
   iconimg.style.height = "80px";
@@ -33,14 +27,10 @@ socials.forEach((item) => {
   p1.style.alignContent = "center";
   p1.style.margin = "0px 30px 30px 30px";
   const button = document.createElement("button");
-  button.style.width = "200px";
-  button.style.height = "50px";
-  button.style.backgroundColor = "black";
-  button.style.color = "white";
-  button.style.borderRadius = "50px";
   button.textContent = "Read more";
-  button.style.textTransform = "UpperCase";
-  button.src = link;
+  socials.forEach(() => {
+    button.onclick = () => window.open((button.src = link), "_blank");
+  });
   div1.append(iconimg, h1, p1, button);
   body.appendChild(div1);
 });
