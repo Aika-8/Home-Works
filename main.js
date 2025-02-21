673a5a4161c252a1e17369f93236a49963a957ff
Aiganysh <dzhumagulova8@gmail.com>
2025-02-21 21:07:29 +0300

    finishing

--- main.js
+++ main.js
@@ -1,119 +1,81 @@
-const div = document.getElementById("firstDiv");
 const inputText = document.getElementById("textInp");
 const button = document.getElementById("addBtn");
 const ul = document.getElementById("todo_list");
-// let getData = localStorage.getItem("texts");
-// if (getData !== "" && getData !== null) {
-//   texts = JSON.parse(getData);
-// }
-let array = [];
+const modalWindow = document.getElementById("modalWindow");
+const yes = document.getElementById("yes");
+const cancel = document.getElementById("cancel");
+let array = JSON.parse(localStorage.getItem("texts")) || [];
+renderTodos();
 button.addEventListener("click", (event) => {
   event.preventDefault();
   const inputValues = inputText.value.trim();
-  if (inputText.value === "") {
+  if (!inputValues) {
     alert("Can't be empty!");
-  } else {
-    const newTodo = {
-      id: Date.now().toString(),
-      title: inputValues,
-    };
-    array.push(newTodo);
-    inputText.value = "";
-    todo(array);
+    return;
   }
+  const newTodo = {
+    id: Date.now().toString(),
+    title: inputValues,
+    completed: false,
+  };
+  array.push(newTodo);
+  inputText.value = "";
+  saveAndRender();
 });
-function todo(data) {
+function renderTodos() {
   ul.innerHTML = "";
-  return data.map((item) => {
+  array.forEach((item) => {
     const li = document.createElement("li");
-    li.textContent = item.title;
+    const textSpan = document.createElement("span");
+    textSpan.textContent = item.title;
+    if (item.completed) {
+      textSpan.style.textDecoration = "line-through";
+      textSpan.style.textDecorationColor = "red";
+    } else {
+      textSpan.style.textDecoration = "none";
+    }
     const checkBox = document.createElement("input");
     checkBox.type = "checkbox";
+    checkBox.checked = item.completed;
+    checkBox.addEventListener("change", () => {
+      item.completed = checkBox.checked;
+      saveAndRender();
+    });
     const btn = document.createElement("button");
     btn.className = "deleting";
     btn.textContent = "delete";
+    btn.addEventListener("click", () => confirmDeletion(item.id));
     const container = document.createElement("div");
-    container.className = "container";
-    container.append(checkBox, btn);
+    container.className = "container_texts";
+    const container1 = document.createElement("div");
+    container1.className = "container_textSpan";
+    const container2 = document.createElement("div");
+    container2.className = "container_check-btn";
+    container1.appendChild(textSpan);
+    container2.append(checkBox, btn);
+    container.append(container1, container2);
     li.appendChild(container);
     ul.appendChild(li);
-    // localStorage.setItem("texts", JSON.stringify(array));
-    window.addEventListener("offline", () => {
-      localStorage.setItem("texts", JSON.stringify(array));
-    });
-    window.addEventListener("online", () => {
-      localStorage.clear();
-    });
-    checkBox.addEventListener("change", () => {
-      if (checkBox.checked) {
-        li.style.textDecoration = "line-through";
-        li.style.textDecorationColor = "red";
-      } else {
-        li.style.textDecoration = "none";
-      }
-    });
-    btn.addEventListener("click", () => {
-      const modalWindow = document.getElementById("modalWindow");
-      modalWindow.style.display = "block";
-      const yes = document.getElementById("yes");
-      const cancel = document.getElementById("cancel");
-      yes.addEventListener("click", () => {
-        li.remove();
-        modalWindow.style.display = "none";
-      });
-      cancel.addEventListener("click", () => {
-        modalWindow.style.display = "none";
-      });
-    });
   });
 }
-
-// button.addEventListener("click", () => {
-//   if (inputText.value === "") {
-//     alert("Can't be empty!");
-//   } else {
-//     const checkBox = document.createElement("input");
-//     checkBox.type = "checkbox";
-//     const btn = document.createElement("button");
-//     btn.className = "deleting";
-//     btn.textContent = "delete";
-//     const inputValues = inputText.value.trim();
-//     const pTag = document.createElement("p");
-//     pTag.className = "pTag";
-//     pTag.textContent = inputValues;
-//     const container = document.createElement("div");
-//     container.className = "container";
-//     container.append(checkBox, btn);
-//     pTag.appendChild(container);
-//     div.appendChild(pTag);
-//     inputText.value = "";
-//     window.addEventListener("offline", () => {
-//       localStorage.setItem("texts", inputValues);
-//     });
-//     window.addEventListener("online", () => {
-//       localStorage.clear();
-//     });
-//     checkBox.addEventListener("change", () => {
-//       if (checkBox.checked) {
-//         pTag.style.textDecoration = "line-through";
-//         pTag.style.textDecorationColor = "red";
-//       } else {
-//         pTag.style.textDecoration = "none";
-//       }
-//     });
-//     btn.addEventListener("click", () => {
-//       const modalWindow = document.getElementById("modalWindow");
-//       modalWindow.style.display = "block";
-//       const yes = document.getElementById("yes");
-//       const cancel = document.getElementById("cancel");
-//       yes.addEventListener("click", () => {
-//         pTag.remove();
-//         modalWindow.style.display = "none";
-//       });
-//       cancel.addEventListener("click", () => {
-//         modalWindow.style.display = "none";
-//       });
-//     });
-//     array.push(localStorage.setItem("texts", inputValues));
-//   }
-// });
+function confirmDeletion(id) {
+  modalWindow.style.display = "block";
+  yes.onclick = () => {
+    array = array.filter((todo) => todo.id !== id);
+    saveAndRender();
+    modalWindow.style.display = "none";
+  };
+  cancel.onclick = () => {
+    modalWindow.style.display = "none";
+  };
+}
+function saveAndRender() {
+  localStorage.setItem("texts", JSON.stringify(array));
+  renderTodos();
+}
+window.addEventListener("offline", () => {
+  localStorage.setItem("texts", JSON.stringify(array));
+});
+window.addEventListener("online", () => {
+  localStorage.removeItem("texts");
+});
